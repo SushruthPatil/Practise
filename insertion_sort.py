8f7403b156159079c8d0b2f96c242b9c654121db
SUSHRUTA  PATIL <104083345+SushruthPatil@users.noreply.github.com>
2023-09-26 17:41:28 +0530

    Add files via upload

---
+++ insertion_sort.py
@@ -0,0 +1,18 @@
+def insertion(arr):
+    n = len(arr)
+
+    for i in range(1,n):
+        key = arr[i]
+        j = i-1
+        
+        while j >= 0 and key<arr[j]:
+            arr[j+1]=arr[j]
+            j-=1
+            
+        arr[j+1] = key
+        
+
+arr = [10,30,40,20]
+insertion(arr)
+print(arr)
+
