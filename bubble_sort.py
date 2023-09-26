7c9550ab80d55c2ebdc436617544db52e7e8fdbf
SUSHRUTA  PATIL <104083345+SushruthPatil@users.noreply.github.com>
2023-09-26 18:05:38 +0530

    Add files via upload

---
+++ bubble_sort.py
@@ -0,0 +1,17 @@
+def bubble(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0,n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+                swapped = True
+        if not swapped:
+            break
+
+arr = [40,10,30,20]
+bubble(arr)
+print(arr)
+
+
+    
